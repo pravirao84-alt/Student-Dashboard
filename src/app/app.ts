7b0d0927442de7ce32066b9b6acbec10f7911f92
P Ravi Rao <pravirao84@gmail.com>
2026-08-08 19:28:18 +0530

    completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,77 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+name: string;
+completed: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [CommonModule, FormsModule],
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Student-Dashboard');
+
+newTask: string = '';
+
+tasks: Task[] = [
+{
+name: 'Complete Angular assignment',
+completed: true
+},
+{
+name: 'Learn Angular routing',
+completed: false
+},
+{
+name: 'Build Student Dashboard',
+completed: false
+}
+];
+
+get completedTasks(): number {
+return this.tasks.filter(task => task.completed).length;
+}
+
+get pendingTasks(): number {
+return this.tasks.filter(task => !task.completed).length;
+}
+
+get progress(): number {
+if (this.tasks.length === 0) {
+return 0;
+}
+
+
+return Math.round(
+  (this.completedTasks / this.tasks.length) * 100
+);
+
+}
+
+addTask(): void {
+const taskName = this.newTask.trim();
+
+
+if (taskName === '') {
+  return;
+}
+
+this.tasks.push({
+  name: taskName,
+  completed: false
+});
+
+this.newTask = '';
+
+
+}
+
+toggleTask(index: number): void {
+this.tasks[index].completed =
+!this.tasks[index].completed;
+}
 }
